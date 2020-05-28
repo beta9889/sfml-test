58d001536bf4d0938f1a92eb6cd0ab1ceb1abbdb
beviseta@gmail.com <beviseta@gmail.com>
2020-05-28 13:31:36 +0200

    loop battle, doesn't show HP

--- test/main.cpp
+++ test/main.cpp
@@ -3,9 +3,16 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+
 using namespace std;
 
 //---------------------------------------------------------------------------
+
+struct battle{
+
+	sf::Uint8 opponentHP = 10;
+	sf::Uint8 playerHP = 10;
+};
 
 void server (int port){
 	
@@ -21,52 +28,60 @@ void server (int port){
 		cout << "error connecting "<< endl;
 	}
 
-	cout << "function finished, connected?\n";
+	cout << "function finished, connected\n";
 
-	cout << "what message do you want to send?\n";
 
 	string input;
 	sf::Packet message;
-	getline(cin >> ws ,input);
+	battle battle;	
 	
-	//cout << input << endl;
+	for (int i=0; i<10;i++){
+		cout << "what message do you want to send?\n";
 
-	if(message << input){
+		getline(cin >> ws ,input);
 		
-		cout << "message in packet\n";
-	}
-	else{
-		cout << "package could not recieve info \n";
-	}
-
-	
-	//cin.getline(message,sizeof(message));
-
-	if(Koppling.send(message) != sf::Socket::Done){
-
-		cout << "error sending message\n";
-	}
-	else {
-		cout << "it worked\n";
+		cout <<"Player Hp = "<< battle.playerHP << endl;
+		cout <<"Opponent HP = "<< battle.opponentHP << endl;
 		
-	}
-	
-	cout << "Waiting for a responce\n";
-	message.clear();
 
-	if(Koppling.receive(message) != sf::Socket::Done){
-
-		cout << "there was a issue\n";
-	}
-	else{
-		if(message >> input){ 
-			message.clear();	
-	
-			cout <<"you got a responce\n"<< input<<endl;
+		if(message << input){
+			if(message << battle.playerHP << battle.opponentHP) 
+				cout << "message in packet\n";
 		}
-	}
-			
+		else{
+			cout << "package could not recieve info \n";
+		}
+
+
+		if(Koppling.send(message) != sf::Socket::Done){
+
+			cout << "error sending message\n";
+		}
+		else {
+			cout << "message sent\n";
+		
+		}
 	
+		cout << "Waiting for a responce\n";
+		message.clear();
+
+		if(Koppling.receive(message) != sf::Socket::Done){
+
+			cout << "there was a issue\n";
+		}
+		else{
+			if(message >> input){ 
+				message.clear();	
+	
+				cout <<"you got a responce\n"<< input<<endl;
+			}
+		}
+			
+		battle.playerHP =battle.playerHP - 2;
+		battle.opponentHP = battle.opponentHP - 2;
+		if (battle.playerHP <= 0) break;
+		else if (battle.opponentHP <= 0) break;
+	}
 
 	cout << "enter e to exit the program\n";
 	cin.ignore(1000,'e');
@@ -91,32 +106,37 @@ void client (int port){
 	
 	sf::Packet message;
 	string output;
-
-	if(socket.receive(message) != sf::Socket::Done){
-		cout << "error recieving message\n";
-	}
-	else{
-		if(message >> output){
-			cout<<" här är medelandet  \"" << output <<"\" "<<endl;
+	battle Cbattle;
+	for (int i=0;i<10;i++){
+		if(socket.receive(message) != sf::Socket::Done){
+			cout << "error recieving message\n";
 		}
+		else{
+			if(message >> output >> Cbattle.opponentHP >> Cbattle.playerHP ){
+				cout<<" här är medelandet  \"" << output <<"\" "<<endl;
+				cout << "player HP = "<< Cbattle.playerHP << endl;
+				cout << "opponent HP = " <<Cbattle.opponentHP << endl;
+			}
 
-	}
+		}
 	
-	message.clear();
+		message.clear();
 
-	cout << "what do you want to respond with?\n";
-	string input;
-	getline(cin >>ws, input);
+		cout << "what do you want to respond with?\n";
+		string input;
+		getline(cin >>ws, input);
 
-	if(message << input){
-		socket.send(message);
-		cout << "message sent\n";
-	}
-	else{
-		cout << "there was a error\n";
-	}
+		if(message << input){
+			socket.send(message);
+			cout << "message sent\n";
+		}
+		else{
+			cout << "there was a error\n";
+		}
 	
-
+		if (Cbattle.playerHP <= 0) break;
+		else if (Cbattle.opponentHP <= 0) break;
+	}
 	cout <<"enter e to exit the program" <<endl;
 	cin.ignore(1000,'e');
 	socket.disconnect();
